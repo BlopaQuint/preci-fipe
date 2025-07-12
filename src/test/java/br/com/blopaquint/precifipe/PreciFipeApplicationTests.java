08f7d9e4b7854593e148ef6ed2548ef9eb5803fc
Pablo Quintiliano <pabloquintiliano@live.com>
2025-07-12 19:19:41 -0300

    Integra lógica com execução Spring Boot

---
+++ src/test/java/br/com/blopaquint/precifipe/PreciFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blopaquint.precifipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreciFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
